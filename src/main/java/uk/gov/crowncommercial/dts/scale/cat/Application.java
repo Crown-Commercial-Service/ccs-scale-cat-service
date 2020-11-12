a5abceef3cb289ae7d7ba7ceb6bcaab41487c195
tommyb82 <thomasbunting@gmail.com>
2020-11-12 13:52:45 +0000

    Basic API skeleton including CF manifest and TravisCI config

---
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/Application.java
@@ -0,0 +1,13 @@
+package uk.gov.crowncommercial.dts.scale.cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
