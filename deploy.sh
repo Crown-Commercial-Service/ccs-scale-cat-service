ddd6cf463efe9dc139644ac23ff562466c1fc7f7
tommyb82 <thomasbunting@gmail.com>
2020-11-17 12:18:26 +0000

    Skip cleanup to ensure target directory not removed after build

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ set -meo pipefail
 pwd
 ls -altr
 cf --version
-
 cf api https://api.london.cloud.service.gov.uk
 cf login -u $CLOUDFOUNDRY_USERNAME -p $CLOUDFOUNDRY_PASSWORD -o ccs-scale-cat -s $CF_SPACE
 cf push --var route-prefix=$CF_ROUTE_PREFIX --var ram=$CF_RAM
