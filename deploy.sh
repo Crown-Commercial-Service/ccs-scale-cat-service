5e79789e9f8ed3433377d2204f454096464c8a89
tommyb82 <thomasbunting@gmail.com>
2020-11-17 12:33:55 +0000

    Tidy up

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+#
+# Login to GPaaS London Cloud Foundry ccs-scale-cat org with space determined by environment variable CF_SPACE.
+# Push local app based on manifest, setting vars `route-prefix` from env var `CF_ROUTE_PREFIX` and `memory` from `CF_MEMORY` 
+#
+
 set -meo pipefail
 
-pwd
-ls -altr
 cf --version
 cf api https://api.london.cloud.service.gov.uk
 cf login -u $CLOUDFOUNDRY_USERNAME -p $CLOUDFOUNDRY_PASSWORD -o ccs-scale-cat -s $CF_SPACE
-cf push --var route-prefix=$CF_ROUTE_PREFIX --var ram=$CF_RAM
+cf push --var route-prefix=$CF_ROUTE_PREFIX --var memory=$CF_MEMORY
