e4e2f0bf1431be17c7099291ed19b839f455c921
tommyb82 <thomasbunting@gmail.com>
2020-11-18 13:55:07 +0000

    First attempt at injecting secure env var to service

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/Application.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/Application.java
@@ -1,16 +1,22 @@
 package uk.gov.crowncommercial.dts.scale.cat;
 
 import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.rollbar.notifier.Rollbar;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@Slf4j
 public class Application {
 
   private final Rollbar rollbar;
+
+  @Value("${AGREEMENTS_SERVICE_URL}")
+  private String agrementsServiceUrl;
 
   public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
@@ -18,6 +24,7 @@ public class Application {
 
   @PostConstruct
   public void init() {
+    log.info("AGREEMENTS_SERVICE_URL=" + agrementsServiceUrl);
     rollbar.debug("CCS Scale CaT API service started...");
   }
 
