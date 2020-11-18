561096903fe6f1bbea3d5d494401992e21d17384
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2020-11-18 09:39:30 +0000

    Feature/sinf 198 rollbar integration (#4)
    * Rollbar configuration (creds required)
    
    * Prefix app name with route-prefix for consistency.
    
    Also add default dummy rollbar access token

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/Application.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/Application.java
@@ -1,13 +1,24 @@
 package uk.gov.crowncommercial.dts.scale.cat;
 
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.rollbar.notifier.Rollbar;
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  private final Rollbar rollbar;
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @PostConstruct
+  public void init() {
+    rollbar.debug("CCS Scale CaT API service started...");
+  }
 
 }
