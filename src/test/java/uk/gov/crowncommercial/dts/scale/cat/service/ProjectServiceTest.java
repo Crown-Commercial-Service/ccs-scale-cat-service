6e92f655f7b8d378c71910fad9390853f5285170
tommyb82 <thomasbunting@gmail.com>
2021-04-27 16:06:22 +0100

    Make test suite pass for build

--- src/test/java/uk/gov/crowncommercial/dts/scale/cat/service/ProjectServiceTest.java
+++ src/test/java/uk/gov/crowncommercial/dts/scale/cat/service/ProjectServiceTest.java
@@ -4,22 +4,25 @@ import javax.annotation.PostConstruct;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import uk.gov.crowncommercial.dts.scale.cat.config.JaggaerOAuth2Config;
+import uk.gov.crowncommercial.dts.scale.cat.config.JaggaerAPIConfig;
 
 /**
  * Service layer tests
  */
-@SpringBootTest(classes = {JaggaerOAuth2Config.class, ProjectService.class})
-public class ProjectServiceTest {
+@SpringBootTest(classes = {ProjectService.class, JaggaerAPIConfig.class},
+    webEnvironment = WebEnvironment.NONE)
+class ProjectServiceTest {
+
+  @MockBean
+  private WebClient jaggaerWebClient;
 
   @Autowired
-  private ProjectService agreementService;
-
-  @Autowired
-  private RestTemplate restTemplate;
+  private ProjectService projectService;
 
   private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -27,11 +30,11 @@ public class ProjectServiceTest {
 
   @PostConstruct
   public void init() {
-    mockServer = MockRestServiceServer.createServer(restTemplate);
+    // mockServer = MockRestServiceServer.createServer(restTemplate);
   }
 
   @Test
-  public void testFindAll() throws Exception {
+  void testFindAll() throws Exception {
 
     // final AgreementSummary[] svcAgreementSummaries =
     // {new AgreementSummary("RM3733", "Technology Products 2"),
