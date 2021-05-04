b7a2b0a4d7090b7a6d2b604fd2295cad2a0643c3
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2021-05-04 16:10:52 +0100

    Initial wiring (inc. SCC-480) (#9)
    * Some basic OAuth config/scaffolding
    
    * Interim commit all resources inc sec
    
    * Temp removal of authority based auth (doesn't work)
    
    * Interim - Jaggaer client models and webclient config
    
    * interim
    
    * Remove unnecessary local copies of API specs
    
    * Split OpenAPI generator into two executions
    
    * Compiles against CaT OpenAPI spec. Jaggaer client config
    
    * Jaggaer auth via OAuth2 client config working
    
    * Update for basic Jaggaer create project
    
    * Create project basic impl working
    
    * Make test suite pass for build
    
    * Fix ProjectService

---
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/JaggaerAPIConfig.java
@@ -0,0 +1,20 @@
+package uk.gov.crowncommercial.dts.scale.cat.config;
+
+import java.util.Map;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import lombok.Data;
+
+/**
+ *
+ */
+@Configuration
+@ConfigurationProperties(prefix = "config.external.jaggaer", ignoreUnknownFields = true)
+@Data
+public class JaggaerAPIConfig {
+
+  private String baseUrl;
+  private Map<String, String> createProject;
+  private Map<String, String> createEvent;
+
+}
---
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/JaggaerOAuth2Config.java
@@ -0,0 +1,81 @@
+package uk.gov.crowncommercial.dts.scale.cat.config;
+
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.endpoint.DefaultClientCredentialsTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2ClientCredentialsGrantRequest;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Configure and expose a non-reactive Jaggaer {@link WebClient} instance for use in calls to
+ * Jaggaer. See
+ * https://docs.spring.io/spring-security/site/docs/5.2.1.RELEASE/reference/htmlsingle/#oauth2Client-webclient-servlet
+ */
+@Configuration
+@RequiredArgsConstructor
+public class JaggaerOAuth2Config {
+
+  private final JaggaerAPIConfig jaggaerAPIConfig;
+  private final JaggaerTokenResponseConverter jaggaerTokenResponseConverter;
+
+  @Bean
+  public OAuth2AccessTokenResponseClient<OAuth2ClientCredentialsGrantRequest> accessTokenResponseClient() {
+
+    DefaultClientCredentialsTokenResponseClient accessTokenResponseClient =
+        new DefaultClientCredentialsTokenResponseClient();
+
+    OAuth2AccessTokenResponseHttpMessageConverter tokenResponseHttpMessageConverter =
+        new OAuth2AccessTokenResponseHttpMessageConverter();
+    tokenResponseHttpMessageConverter.setTokenResponseConverter(jaggaerTokenResponseConverter);
+
+    RestTemplate restTemplate = new RestTemplate(
+        Arrays.asList(new FormHttpMessageConverter(), tokenResponseHttpMessageConverter));
+    restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+
+    accessTokenResponseClient.setRestOperations(restTemplate);
+    return accessTokenResponseClient;
+  }
+
+  @Bean
+  public OAuth2AuthorizedClientManager authorizedClientManager(
+      ClientRegistrationRepository clientRegistrationRepository,
+      OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+    OAuth2AuthorizedClientProvider authorizedClientProvider =
+        OAuth2AuthorizedClientProviderBuilder.builder()
+            .clientCredentials(
+                configurer -> configurer.accessTokenResponseClient(accessTokenResponseClient()))
+            .build();
+
+    DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+        new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+            authorizedClientRepository);
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+    return authorizedClientManager;
+  }
+
+  @Bean("jaggaerWebClient")
+  public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+    ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+        new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+    oauth2Client.setDefaultClientRegistrationId("jaggaer");
+    return WebClient.builder().baseUrl(jaggaerAPIConfig.getBaseUrl())
+        .apply(oauth2Client.oauth2Configuration()).build();
+  }
+
+}
