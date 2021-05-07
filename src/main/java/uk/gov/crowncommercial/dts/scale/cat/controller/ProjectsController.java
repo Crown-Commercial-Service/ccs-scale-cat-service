dd940a2bf93dd1f936ecc9c3907d4263439ef238
tommyb82 <thomasbunting@gmail.com>
2021-05-07 09:58:38 +0100

    Remove Rollbar config

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/controller/ProjectsController.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/controller/ProjectsController.java
@@ -4,7 +4,6 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.rollbar.notifier.Rollbar;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import uk.gov.crowncommercial.dts.scale.cat.model.generated.AgreementDetails;
@@ -19,14 +18,12 @@ import uk.gov.crowncommercial.dts.scale.cat.service.ProcurementProjectService;
 @Slf4j
 public class ProjectsController {
 
-  private final Rollbar rollbar;
   private final ProcurementProjectService procurementProjectService;
 
   // @PreAuthorize("hasAuthority('ORG_ADMINISTRATOR')")
   @PostMapping("/tenders/projects/agreements")
   public DraftProcurementProject createProcurementProject(
       @RequestBody AgreementDetails agreementDetails, JwtAuthenticationToken authentication) {
-    rollbar.debug("POST createProcurementProject invoked");
 
     String principal = authentication.getTokenAttributes().get("sub").toString();
     log.info("createProcuremenProject invoked on bahelf of principal: {}", principal);
