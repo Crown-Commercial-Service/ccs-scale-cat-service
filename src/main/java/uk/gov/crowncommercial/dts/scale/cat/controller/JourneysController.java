824d1917c94a7791be27d2749da9556afcf1bdc3
tommyb82 <thomasbunting@gmail.com>
2021-12-06 16:06:41 +0000

    Journey service classes - working

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/controller/JourneysController.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/controller/JourneysController.java
@@ -2,11 +2,9 @@ package uk.gov.crowncommercial.dts.scale.cat.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import java.util.Collection;
-import java.util.Set;
-import javax.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import uk.gov.crowncommercial.dts.scale.cat.model.journey_service.generated.Journey;
 import uk.gov.crowncommercial.dts.scale.cat.model.journey_service.generated.JourneyStepState;
 import uk.gov.crowncommercial.dts.scale.cat.model.journey_service.generated.StepState;
@@ -18,25 +16,26 @@ import uk.gov.crowncommercial.dts.scale.cat.service.JourneyService;
 @RestController
 @RequestMapping(path = "/journeys", produces = APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
-public class JourneysController {
+@Slf4j
+public class JourneysController extends AbstractRestController {
 
   private final JourneyService journeyService;
 
   @PostMapping
   public String createJourney(@RequestBody final Journey journey) {
-    return "journey-abc";
+    return journeyService.createJourney(journey);
   }
 
   @GetMapping("/{journey-id}/steps")
   public Collection<JourneyStepState> getJourneyState(
-      @PathParam("journey-id") final String journeyId) {
-    return Set.of(new JourneyStepState());
+      @PathVariable("journey-id") final String journeyId) {
+    return journeyService.getJourneyState(journeyId);
   }
 
   @PutMapping("/{journey-id}/steps/{step-id}")
-  public StepState updateJourneyStepState(@PathParam("journey-id") final String journeyId,
-      @PathParam("step-id") final Integer stepId) {
-    return StepState.NOT_STARTED;
+  public void updateJourneyStepState(@PathVariable("journey-id") final String journeyId,
+      @PathVariable("step-id") final Integer stepId, @RequestBody final StepState stepState) {
+    journeyService.updateJourneyStepState(journeyId, stepId, stepState);
   }
 
 }
--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/JourneyService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/JourneyService.java
@@ -1,12 +1,16 @@
 package uk.gov.crowncommercial.dts.scale.cat.service;
 
+import java.time.Instant;
 import java.util.Collection;
-import java.util.Set;
+import java.util.Objects;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import uk.gov.crowncommercial.dts.scale.cat.exception.ResourceNotFoundException;
+import uk.gov.crowncommercial.dts.scale.cat.model.entity.JourneyEntity;
 import uk.gov.crowncommercial.dts.scale.cat.model.journey_service.generated.Journey;
 import uk.gov.crowncommercial.dts.scale.cat.model.journey_service.generated.JourneyStepState;
 import uk.gov.crowncommercial.dts.scale.cat.model.journey_service.generated.StepState;
+import uk.gov.crowncommercial.dts.scale.cat.repo.JourneyRepo;
 
 /**
  *
@@ -15,16 +19,41 @@ import uk.gov.crowncommercial.dts.scale.cat.model.journey_service.generated.Step
 @RequiredArgsConstructor
 public class JourneyService {
 
+  private static final String ERR_FMT_JOURNEY_NOT_FOUND = "Journey [%s] not found";
+  private final JourneyRepo journeyRepo;
+
   public String createJourney(final Journey journey) {
-    return "journey-abc";
+
+    var journeyEntity = JourneyEntity.builder().clientId("CAT_BUYER_UI")
+        .externalId(journey.getJourneyId()).journeyDetails(journey.getStates()).createdBy("tom")
+        .createdAt(Instant.now()).updatedBy("tom").updatedAt(Instant.now()).build();
+
+    journeyRepo.save(journeyEntity);
+    return journey.getJourneyId();
   }
 
   public Collection<JourneyStepState> getJourneyState(final String journeyId) {
-    return Set.of(new JourneyStepState());
+    var journey = findJourney(journeyId);
+    return journey.getJourneyDetails();
   }
 
-  public StepState updateJourneyStepState(final String journeyId, final Integer stepId) {
-    return StepState.NOT_STARTED;
+  public void updateJourneyStepState(final String journeyId, final Integer stepId,
+      final StepState stepState) {
+    var journey = findJourney(journeyId);
+    var journeyStepStates = journey.getJourneyDetails();
+
+    var journeyStepState = journeyStepStates.stream()
+        .filter(jss -> Objects.equals(stepId, jss.getStep())).findFirst().orElseThrow();
+    journeyStepState.setState(stepState);
+
+    journeyRepo.save(journey);
+  }
+
+  private JourneyEntity findJourney(final String journeyId) {
+    var journey = journeyRepo.findByExternalId(journeyId);
+
+    return journey.orElseThrow(
+        () -> new ResourceNotFoundException(String.format(ERR_FMT_JOURNEY_NOT_FOUND, journeyId)));
   }
 
 }
