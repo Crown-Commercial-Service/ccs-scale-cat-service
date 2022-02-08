61fb99c3a55553ed4e51bd43b88970e172995d97
Sudhakar Konda <sudhakar.konda@crowncommercial.gov.uk>
2022-02-08 09:28:39 +0000

    SCAT-2616 code review changes

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/agreements/Dependency.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/agreements/Dependency.java
@@ -1,17 +1,15 @@
 package uk.gov.crowncommercial.dts.scale.cat.model.agreements;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
-import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Value
 @Builder
 @Jacksonized
-@Slf4j
 public class Dependency{
-	Conditional conditional;
-	List<Relationships> relationships;
+  Conditional conditional;
+  List<Relationships> relationships;
 }
--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/agreements/Relationships.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/agreements/Relationships.java
@@ -3,14 +3,12 @@ package uk.gov.crowncommercial.dts.scale.cat.model.agreements;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
-import lombok.extern.slf4j.Slf4j;
 
 @Value
 @Builder
 @Jacksonized
-@Slf4j
 public class Relationships{
-	String dependentOnID;
-	String relationshipType;
+  String dependentOnID;
+  String relationshipType;
 }
 
