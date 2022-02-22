dca7565ddc65470438921e8794df230112047bbf
venkibric <venki.bathula@brickendon.com>
2022-02-22 00:43:13 +0000

    created message controller and service

---
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/rpa/RPAGenericData.java
@@ -0,0 +1,23 @@
+package uk.gov.crowncommercial.dts.scale.cat.model.rpa;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Accessors(chain = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RPAGenericData {
+	String processInput;
+	String processName;
+	String profileName;
+	String source;
+	String sourceId;
+	boolean retry;
+	@JsonProperty(value = "isSync")
+	boolean isSync;
+	long requestTimeout;
+}
---
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/rpa/RetryConfiguartion.java
@@ -0,0 +1,16 @@
+package uk.gov.crowncommercial.dts.scale.cat.model.rpa;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Jacksonized
+@Builder
+public class RetryConfiguartion {
+	List<String> previousRequestIds;
+	int noOfTimesRetry;
+	long retryCoolOffInterval;
+}
