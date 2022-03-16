a4cf527b8bc4190030d33d948ee0316115a73aaa
venkibric <venki.bathula@brickendon.com>
2022-03-16 14:46:58 +0000

    changed json parser

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/RPAGenericService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/RPAGenericService.java
@@ -1,22 +1,17 @@
 package uk.gov.crowncommercial.dts.scale.cat.service;
 
 import static org.springframework.util.CollectionUtils.isEmpty;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -25,11 +20,7 @@ import uk.gov.crowncommercial.dts.scale.cat.exception.JaggaerRPAException;
 import uk.gov.crowncommercial.dts.scale.cat.model.entity.OrganisationMapping;
 import uk.gov.crowncommercial.dts.scale.cat.model.entity.ProcurementEvent;
 import uk.gov.crowncommercial.dts.scale.cat.model.jaggaer.Supplier;
-import uk.gov.crowncommercial.dts.scale.cat.model.rpa.AutomationOutputData;
-import uk.gov.crowncommercial.dts.scale.cat.model.rpa.RPAAPIResponse;
-import uk.gov.crowncommercial.dts.scale.cat.model.rpa.RPAGenericData;
-import uk.gov.crowncommercial.dts.scale.cat.model.rpa.RPAProcessInput;
-import uk.gov.crowncommercial.dts.scale.cat.model.rpa.RPAProcessNameEnum;
+import uk.gov.crowncommercial.dts.scale.cat.model.rpa.*;
 import uk.gov.crowncommercial.dts.scale.cat.repo.RetryableTendersDBDelegate;
 
 @Service
@@ -49,42 +40,48 @@ public class RPAGenericService {
    * @param nonOCDS
    * @return suppliers as a string
    */
-  public Pair<List<Supplier>, Set<OrganisationMapping>> getValidSuppliers(final ProcurementEvent procurementEvent,
-      final List<String> orgIds) {
+  public Pair<List<Supplier>, Set<OrganisationMapping>> getValidSuppliers(
+      final ProcurementEvent procurementEvent, final List<String> orgIds) {
     // ignoring string content from organisation Ids
-    var supplierOrgIds = orgIds.stream().map(ls -> ls.replace("GB-COH-", "")).collect(Collectors.toSet());
+    var supplierOrgIds =
+        orgIds.stream().map(ls -> ls.replace("GB-COH-", "")).collect(Collectors.toSet());
 
     // Retrieve and verify Tenders DB org mappings
-    var supplierOrgMappings = retryableTendersDBDelegate.findOrganisationMappingByOrganisationIdIn(supplierOrgIds);
+    var supplierOrgMappings =
+        retryableTendersDBDelegate.findOrganisationMappingByOrganisationIdIn(supplierOrgIds);
     if (isEmpty(supplierOrgMappings)) {
-      var errorDesc = String.format("No supplier organisation mappings found in Tenders DB %s", supplierOrgIds);
+      var errorDesc =
+          String.format("No supplier organisation mappings found in Tenders DB %s", supplierOrgIds);
       log.error(errorDesc);
       throw new JaggaerRPAException(errorDesc);
     }
 
     // Find all externalOrgIds
-    var supplierExternalIds = supplierOrgMappings.stream().map(OrganisationMapping::getExternalOrganisationId)
-        .collect(Collectors.toSet());
+    var supplierExternalIds = supplierOrgMappings.stream()
+        .map(OrganisationMapping::getExternalOrganisationId).collect(Collectors.toSet());
 
     // Find actual suppliers of project and event
-    var suppliers = jaggaerService.getRfx(procurementEvent.getExternalEventId()).getSuppliersList().getSupplier();
+    var suppliers = jaggaerService.getRfx(procurementEvent.getExternalEventId()).getSuppliersList()
+        .getSupplier();
 
     // Find all unmatched org ids
     var unMatchedSuppliers = supplierExternalIds.stream()
-        .filter(orgid -> suppliers.stream().noneMatch(supplier -> supplier.getCompanyData().getId().equals(orgid)))
+        .filter(orgid -> suppliers.stream()
+            .noneMatch(supplier -> supplier.getCompanyData().getId().equals(orgid)))
         .collect(Collectors.toList());
 
     if (!isEmpty(unMatchedSuppliers)) {
-      var errorDesc = String.format("Supplied organisation mappings not matched with actual suppliers '%s'",
-          unMatchedSuppliers);
+      var errorDesc =
+          String.format("Supplied organisation mappings not matched with actual suppliers '%s'",
+              unMatchedSuppliers);
       log.error(errorDesc);
       throw new JaggaerRPAException(errorDesc);
     }
 
     // Comparing the requested organisation ids and event suppliers info
     var matchedSuppliers = suppliers.stream()
-        .filter(
-            supplier -> supplierExternalIds.stream().anyMatch(orgId -> orgId.equals(supplier.getCompanyData().getId())))
+        .filter(supplier -> supplierExternalIds.stream()
+            .anyMatch(orgId -> orgId.equals(supplier.getCompanyData().getId())))
         .collect(Collectors.toList());
 
     return Pair.of(matchedSuppliers, supplierOrgMappings);
@@ -96,15 +93,18 @@ public class RPAGenericService {
    * @throws JsonProcessingException
    */
   @SneakyThrows
-  public String callRPAMessageAPI(final RPAProcessInput processInput, final RPAProcessNameEnum processName) {
+  public String callRPAMessageAPI(final RPAProcessInput processInput,
+      final RPAProcessNameEnum processName) {
     var request = new RPAGenericData();
-    request.setProcessInput(objectMapper.writeValueAsString(processInput)).setProcessName(processName.getValue())
-        .setProfileName(rpaAPIConfig.getProfileName()).setSource(rpaAPIConfig.getSource())
-        .setSourceId(rpaAPIConfig.getSourceId()).setRequestTimeout(rpaAPIConfig.getRequestTimeout()).setSync(true);
+    request.setProcessInput(objectMapper.writeValueAsString(processInput))
+        .setProcessName(processName.getValue()).setProfileName(rpaAPIConfig.getProfileName())
+        .setSource(rpaAPIConfig.getSource()).setSourceId(rpaAPIConfig.getSourceId())
+        .setRequestTimeout(rpaAPIConfig.getRequestTimeout()).setSync(true);
     log.info("RPA Request: {}", objectMapper.writeValueAsString(request));
 
-    var response = webclientWrapper.postDataWithToken(request, RPAAPIResponse.class, rpaServiceWebClient,
-        rpaAPIConfig.getTimeoutDuration(), rpaAPIConfig.getAccessUrl(), getAccessToken());
+    var response =
+        webclientWrapper.postDataWithToken(request, RPAAPIResponse.class, rpaServiceWebClient,
+            rpaAPIConfig.getTimeoutDuration(), rpaAPIConfig.getAccessUrl(), getAccessToken());
 
     return validateResponse(response);
   }
@@ -132,9 +132,9 @@ public class RPAGenericService {
   @SneakyThrows
   private String validateResponse(final RPAAPIResponse apiResponse) {
     var convertedObject = convertStringToObject(apiResponse.getResponse().getResponse());
-    var json = new JSONObject(convertedObject);
-    var jsonArray = json.getJSONArray("AutomationOutputData");
-    var automationData = objectMapper.readValue(jsonArray.get(1).toString(), AutomationOutputData.class);
+    var maps = (List<Map<String, String>>) convertedObject.get("AutomationOutputData");
+    var automationData = objectMapper.readValue(objectMapper.writeValueAsString(maps.get(2)),
+        AutomationOutputData.class);
 
     var status = automationData.getCviewDictionary().getStatus();
     log.info("Status of RPA API call : {} ", status);
@@ -155,7 +155,6 @@ public class RPAGenericService {
    */
   @SneakyThrows
   private Map<String, Object> convertStringToObject(final String inputString) {
-    return objectMapper.readValue(inputString, new TypeReference<HashMap<String, Object>>() {
-    });
+    return objectMapper.readValue(inputString, new TypeReference<HashMap<String, Object>>() {});
   }
 }
