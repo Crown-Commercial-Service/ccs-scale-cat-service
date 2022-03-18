f82db04dd62d2850c60df33354d727d0d303e181
Sudhakar Konda <sudhakar.konda@crowncommercial.gov.uk>
2022-03-18 15:05:04 +0000

    SCAT-4572 Handling to RFX not found from jaggaer

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ProcurementProjectService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ProcurementProjectService.java
@@ -407,7 +407,7 @@ public class ProcurementProjectService {
       projectPackageSummary.activeEvent(eventSummary);
     } catch (Exception e) {
       // No data found in Jagger
-      log.debug("Unable to find RFX records for event id, %s",dbEvent.getExternalEventId());
+      log.debug("Unable to find RFX records for event id : "+dbEvent.getExternalEventId());
     }
     return Optional.of(projectPackageSummary);
   }
