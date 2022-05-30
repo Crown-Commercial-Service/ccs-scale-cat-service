0c7b5b098b92a4108e443627d1bd81a8997a78db
SrinivasaDontiboyina <srinivasa.dontiboyina@brickendon.com>
2022-05-30 14:47:15 +0100

    Refactored code

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/utils/TendersAPIModelUtils.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/utils/TendersAPIModelUtils.java
@@ -132,19 +132,12 @@ public class TendersAPIModelUtils {
       final RfxSetting rfxSetting, final ProcurementEvent procurementEvent) {
 
     var tenderStatus = procurementEvent.getTenderStatus();
-    if (Objects.nonNull(tenderStatus)) {
-      if (tenderStatus.strip().equalsIgnoreCase(COMPLETE_STATUS)) {
-        return DashboardStatus.COMPLETE;
-      } else if (tenderStatus.strip().equalsIgnoreCase(CLOSED_STATUS)
-                || tenderStatus.strip().equalsIgnoreCase(CANCELLED_STATUS) ) {
-        return DashboardStatus.CLOSED;
-      }
-      if (Constants.TENDER_DB_ONLY_EVENT_TYPES.contains(
-              DefineEventType.fromValue(procurementEvent.getEventType()))) {
-        return DashboardStatus.ASSESSMENT;
-      }
-    }
-    else { // No rfx, use procurement event status
+    DashboardStatus dashboardStatusFromTenderStatus =
+        deriveDashboardStatusBasedOnTenderStatus(tenderStatus);
+
+    if (null != dashboardStatusFromTenderStatus) {
+      return dashboardStatusFromTenderStatus;
+    } else { // No rfx, use procurement event status
       if (Constants.TENDER_DB_ONLY_EVENT_TYPES.contains(
           DefineEventType.fromValue(procurementEvent.getEventType()))) {
         return DashboardStatus.ASSESSMENT;
@@ -161,11 +154,23 @@ public class TendersAPIModelUtils {
         } else {
           return evaluateDashboardStatusFromRfxSettingStatus(rfxSetting);
         }
-
       }
     }
     log.error("DashboardStatus is not determined , returning UNKNOWN Status ");
     return DashboardStatus.UNKNOWN;
+  }
+
+  private DashboardStatus deriveDashboardStatusBasedOnTenderStatus(String tenderStatus) {
+
+    if (Objects.nonNull(tenderStatus)) {
+      if (tenderStatus.strip().equalsIgnoreCase(COMPLETE_STATUS)) {
+        return DashboardStatus.COMPLETE;
+      } else if (tenderStatus.strip().equalsIgnoreCase(CLOSED_STATUS)
+          || tenderStatus.strip().equalsIgnoreCase(CANCELLED_STATUS)) {
+        return DashboardStatus.CLOSED;
+      }
+    }
+    return null;
   }
 
   private DashboardStatus evaluateDashboardStatusFromRfxSettingStatus(RfxSetting rfxSetting) {
