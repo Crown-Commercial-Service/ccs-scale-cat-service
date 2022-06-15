0e7ec14b5c217142eaff6495e768d0fc95e5df92
venkibric <98110989+venkibric@users.noreply.github.com>
2022-06-15 13:01:04 +0100

    Feature/scat 0000 migrate templateid ssm (#420)
    added templateid in ssm

--- iac/modules/cat-service/main.tf
+++ iac/modules/cat-service/main.tf
@@ -54,6 +54,10 @@ data "aws_ssm_parameter" "jaggaer_self_service_id" {
   name = "/cat/${var.environment}/jaggaer-self-service-id"
 }
 
+data "aws_ssm_parameter" "jaggaer_itt_template_id" {
+  name = "/cat/${var.environment == "prd" ? "prd" : "default"}/jaggaer-itt-template-id"
+}
+
 # Auth server / CII
 data "aws_ssm_parameter" "auth_server_jwk_set_uri" {
   name = "/cat/${var.environment}/auth-server-jwk-set-uri"
@@ -154,6 +158,7 @@ resource "cloudfoundry_app" "cat_service" {
     "spring.security.oauth2.client.provider.jaggaer.token-uri" : data.aws_ssm_parameter.jaggaer_token_url.value
     "config.external.jaggaer.baseUrl" : data.aws_ssm_parameter.jaggaer_base_url.value
     "config.external.jaggaer.self-service-id" : data.aws_ssm_parameter.jaggaer_self_service_id.value
+    "config.external.jaggaer.createRfx.templateId" : data.aws_ssm_parameter.jaggaer_itt_template_id.value
     
     # Auth server / CII
     "spring.security.oauth2.resourceserver.jwt.jwk-set-uri" : data.aws_ssm_parameter.auth_server_jwk_set_uri.value
