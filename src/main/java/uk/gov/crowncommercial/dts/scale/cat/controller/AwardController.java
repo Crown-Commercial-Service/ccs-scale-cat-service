de6894d0befd5ef66931002e45b06328481f8855
venkibric <venki.bathula@brickendon.com>
2022-07-08 12:41:41 +0100

    added award rfx

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/controller/AwardController.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/controller/AwardController.java
@@ -48,7 +48,7 @@ public class AwardController extends AbstractRestController {
     var principal = getPrincipalFromJwt(authentication);
     var conclaveOrgId = getCiiOrgIdFromJwt(authentication);
     log.info("createOrComplete award process on behalf of principal: {}", principal, conclaveOrgId);
-    return ResponseEntity.ok(awardService.createOrUpdateAward(principal, procId, eventId,
+    return ResponseEntity.ok(awardService.createOrUpdateAwardRfx(principal, procId, eventId,
         awardState, awardRequest, null));
   }
 
