38f71143237ce6c7acac721e8159410c9c7e296f
venkibric <venki.bathula@brickendon.com>
2023-03-16 12:36:07 +0000

    Revert "added filter for get projects based on event date (#1062)"
    This reverts commit 79883ab2676f21b1819e8957bf7e3f644cb42359.

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ProcurementProjectService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ProcurementProjectService.java
@@ -23,8 +23,6 @@ import uk.gov.crowncommercial.dts.scale.cat.utils.TendersAPIModelUtils;
 import javax.transaction.Transactional;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -415,18 +413,18 @@ public class ProcurementProjectService {
                                                       jaggaerUserId,
                                                       searchType,
                                                       searchTerm,
-                                                      PageRequest.of(Objects.nonNull(page)?Integer.valueOf(page):0, Objects.nonNull(pageSize)?Integer.valueOf(pageSize):20, Sort.by("project.procurementEvents.updatedAt").descending()));
+                                                      PageRequest.of(Objects.nonNull(page)?Integer.valueOf(page):0, Objects.nonNull(pageSize)?Integer.valueOf(pageSize):20, Sort.by("timestamps.createdAt").descending()));
 
     if (!CollectionUtils.isEmpty(projectUserMappings)) {
       var externalEventIdsAllProjects = projectUserMappings.stream()
           .flatMap(pum -> pum.getProject().getProcurementEvents().stream())
           .map(ProcurementEvent::getExternalEventId).collect(Collectors.toSet());
+
       var projectUserRfxs = jaggaerService.searchRFx(externalEventIdsAllProjects);
-      
+
       return projectUserMappings.stream()
           .map(pum -> convertProjectToProjectPackageSummary(pum, projectUserRfxs))
-          .filter(Optional::isPresent).map(Optional::get)
-          .collect(Collectors.toSet());
+          .filter(Optional::isPresent).map(Optional::get).collect(Collectors.toSet());
     }
     return Collections.emptyList();
   }
@@ -513,7 +511,6 @@ public class ProcurementProjectService {
 
     if(null != eventSummary) {
       eventSummary.setDashboardStatus(tendersAPIModelUtils.getDashboardStatus(rfxSetting, dbEvent));
-      eventSummary.setLastUpdated(OffsetDateTime.ofInstant(dbEvent.getUpdatedAt(),ZoneId.systemDefault()));
       projectPackageSummary.activeEvent(eventSummary);
     }
 
