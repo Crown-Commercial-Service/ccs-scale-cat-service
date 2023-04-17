22c3df1746a55ffe2361c46f7aab32081a0d7627
Peter Simpson <peter.simpson@roweit.co.uk>
2023-04-17 12:54:25 +0100

    EI-74 Add salesforce endpoint to CAT service - config changes/fixes

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/JaggaerAPIConfig.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/JaggaerAPIConfig.java
@@ -62,6 +62,7 @@ public class JaggaerAPIConfig {
   private Map<String, String> preAward;
   private Map<String, String> completeTechnical;
   private Map<String, String> apiDefaults;
+  private Map<String, String> esourcing;
 
   // Temporary - SOAP config
   private JaggaerSOAPAPIConfig soap;
