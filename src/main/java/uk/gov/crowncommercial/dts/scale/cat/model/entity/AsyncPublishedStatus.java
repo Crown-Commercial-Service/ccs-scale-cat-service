12e346f4f79c98615f03f826fc66dd8b3b83f8ff
venkibric <venki.bathula@brickendon.com>
2023-05-16 14:01:42 +0100

    codeclimate changes

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/entity/AsyncPublishedStatus.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/model/entity/AsyncPublishedStatus.java
@@ -5,13 +5,4 @@ public enum AsyncPublishedStatus {
 
   IN_FLIGHT, SCHEDULED, COMPLETED, FAILED;
 
-  public static AsyncPublishedStatus fromName(final String name) {
-    for (AsyncPublishedStatus b : AsyncPublishedStatus.values()) {
-      if (b.name().equalsIgnoreCase(name)) {
-        return b;
-      }
-    }
-    throw new IllegalArgumentException("Unexpected name '" + name + "'");
-  }
-
 }
--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/utils/TendersAPIModelUtils.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/utils/TendersAPIModelUtils.java
@@ -155,28 +155,36 @@ public class TendersAPIModelUtils {
           && Constants.TENDER_NON_DB_EVENT_TYPES.contains(
               ViewEventType.fromValue(procurementEvent.getEventType()))
           && Objects.nonNull(rfxSetting)) {
-
-        if (Objects.isNull(rfxSetting.getPublishDate())
-            && Objects.isNull(procurementEvent.getAsyncPublishedStatus())) {
-          return DashboardStatus.IN_PROGRESS;
-        } else if (Objects.nonNull(procurementEvent.getAsyncPublishedStatus())
-            && ASYNC_PUBLISH_STATUS_TYPES.contains(
-                AsyncPublishedStatus.fromName(procurementEvent.getAsyncPublishedStatus()))) {
-          return DashboardStatus.PUBLISHING;
-        } else if (Objects.nonNull(procurementEvent.getAsyncPublishedStatus())
-            && AsyncPublishedStatus.FAILED.name()
-                .equalsIgnoreCase(procurementEvent.getAsyncPublishedStatus())) {
-          return DashboardStatus.PUBLISH_FAILED;
-        } else if (Objects.nonNull(rfxSetting.getCloseDate()) 
-            && rfxSetting.getCloseDate().isAfter(OffsetDateTime.now())) {
-          return DashboardStatus.PUBLISHED;
-        } else {
-          return evaluateDashboardStatusFromRfxSettingStatus(rfxSetting);
-        }
+        return deriveAsyncPublishStatus(rfxSetting, procurementEvent);
       }
     }
     log.error("DashboardStatus is not determined , returning UNKNOWN Status ");
     return DashboardStatus.UNKNOWN;
+  }
+  
+  private static DashboardStatus deriveAsyncPublishStatus(
+      final RfxSetting rfxSetting, final ProcurementEvent procurementEvent) {
+
+    DashboardStatus dashboardStatus;
+    if (Objects.isNull(rfxSetting.getPublishDate())
+        && Objects.isNull(procurementEvent.getAsyncPublishedStatus())) {
+      dashboardStatus = DashboardStatus.IN_PROGRESS;
+    } else if (Objects.nonNull(procurementEvent.getAsyncPublishedStatus())
+        && ASYNC_PUBLISH_STATUS_TYPES
+            .contains(AsyncPublishedStatus.valueOf(procurementEvent.getAsyncPublishedStatus()))) {
+      dashboardStatus = DashboardStatus.PUBLISHING;
+    } else if (Objects.nonNull(procurementEvent.getAsyncPublishedStatus())
+        && AsyncPublishedStatus.FAILED.name()
+            .equalsIgnoreCase(procurementEvent.getAsyncPublishedStatus())) {
+      dashboardStatus = DashboardStatus.PUBLISH_FAILED;
+    } else if (Objects.nonNull(rfxSetting.getCloseDate())
+        && rfxSetting.getCloseDate().isAfter(OffsetDateTime.now())) {
+      dashboardStatus = DashboardStatus.PUBLISHED;
+    } else {
+      dashboardStatus = evaluateDashboardStatusFromRfxSettingStatus(rfxSetting);
+    }
+    return dashboardStatus;
+
   }
 
   private static DashboardStatus deriveDashboardStatusBasedOnTenderStatus(String tenderStatus) {
