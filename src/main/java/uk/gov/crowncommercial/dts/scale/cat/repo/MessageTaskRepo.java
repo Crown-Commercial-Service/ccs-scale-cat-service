ef0d1cb0df2a994f784186293adad9225e2d8d91
SAIKUMAR REDDY <saikumar.reddy@brickendon.com>
2023-06-09 10:53:25 +0530

    Query changes for GET messages API

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/repo/MessageTaskRepo.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/repo/MessageTaskRepo.java
@@ -3,6 +3,7 @@ package uk.gov.crowncommercial.dts.scale.cat.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.crowncommercial.dts.scale.cat.model.entity.ca.MessageAsync;
 
@@ -11,6 +12,6 @@ import java.util.List;
 @Repository
 public interface MessageTaskRepo extends JpaRepository<MessageAsync, Integer> {
 
-    @Query("SELECT m FROM message_task m WHERE m.status = 'INPROGRESS'")
-    public List<MessageAsync> findAllByEventId(Integer eventId);
+    @Query("SELECT m FROM MessageAsync m WHERE m.eventId = :eventId AND m.status = 'INPROGRESS'")
+    public List<MessageAsync> findAllByEventId(@Param("eventId") Integer eventId);
 }
