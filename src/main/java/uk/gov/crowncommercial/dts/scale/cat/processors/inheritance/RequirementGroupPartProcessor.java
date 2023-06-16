c83c74c3efd0cd29e87f939035e78cdfba38b4cd
ravi-sankar-uppala <112702827+ravi-sankar-uppala@users.noreply.github.com>
2023-06-16 12:48:14 +0100

    Release to UAT (#1286)
    * Removed duplicate code
    
    * Merge changes
    
    * Added ignore to exclude the fields
    
    * Added conditional field
    
    * Removed the copy values utils and built custom
    
    * Update issue when timeline is null
    
    * Codeclimate fix
    
    * Template changes
    
    * Removed text
    
    * Added the new timelinequestion
    
    * Merged code changes
    
    * updated indicator
    
    * Corrected the merge (#1284) (#1285)
    
    Co-authored-by: Ravi <baby@babys-MacBook-Pro.local>
    
    ---------
    
    Co-authored-by: Ravi <baby@babys-MacBook-Pro.local>
    Co-authored-by: Ravi <baby@ip-192-168-1-30.eu-west-2.compute.internal>
    Co-authored-by: Venki Bathula <98110989+venkibric@users.noreply.github.com>

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/processors/inheritance/RequirementGroupPartProcessor.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/processors/inheritance/RequirementGroupPartProcessor.java
@@ -25,8 +25,7 @@ public class RequirementGroupPartProcessor implements InheritanceProcessor<Requi
                     Requirement question = getQuestion(requirementGroup, req, questions);
                     if(null != question) {
                         req.getNonOCDS().setInheritance(inheritanceType);
-                        if(null != question.getNonOCDS().getOptions())
-                            req.getNonOCDS().updateOptions(question.getNonOCDS().getOptions());
+                        copyOptions(req, question);
                     }else{
                         req.getNonOCDS().setInheritance(null);
                     }
