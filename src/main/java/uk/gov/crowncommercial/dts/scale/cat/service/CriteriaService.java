dcec1dd6309bc2c0a24d498ceb673b699cb0a232
ravi-sankar-uppala <112702827+ravi-sankar-uppala@users.noreply.github.com>
2023-06-20 13:39:48 +0100

    Release to Pre prod (#1295)
    * Release to UAT (#1278)
    
    * Removed duplicate code
    
    * Merge changes
    
    * Added ignore to exclude the fields
    
    * Added conditional field
    
    * Removed the copy values utils and built custom
    
    * Update issue when timeline is null
    
    * Codeclimate fix
    
    * Template changes
    
    * Removed text
    
    * Added the new timelinequestion
    
    * Merged code changes
    
    * updated indicator
    
    ---------
    
    Co-authored-by: Ravi <baby@babys-MacBook-Pro.local>
    Co-authored-by: Ravi <baby@ip-192-168-1-30.eu-west-2.compute.internal>
    Co-authored-by: Venki Bathula <98110989+venkibric@users.noreply.github.com>
    
    * Release to UAT (#1286)
    
    * Removed duplicate code
    
    * Merge changes
    
    * Added ignore to exclude the fields
    
    * Added conditional field
    
    * Removed the copy values utils and built custom
    
    * Update issue when timeline is null
    
    * Codeclimate fix
    
    * Template changes
    
    * Removed text
    
    * Added the new timelinequestion
    
    * Merged code changes
    
    * updated indicator
    
    * Corrected the merge (#1284) (#1285)
    
    Co-authored-by: Ravi <baby@babys-MacBook-Pro.local>
    
    ---------
    
    Co-authored-by: Ravi <baby@babys-MacBook-Pro.local>
    Co-authored-by: Ravi <baby@ip-192-168-1-30.eu-west-2.compute.internal>
    Co-authored-by: Venki Bathula <98110989+venkibric@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ravi <baby@babys-MacBook-Pro.local>
    Co-authored-by: Ravi <baby@ip-192-168-1-30.eu-west-2.compute.internal>
    Co-authored-by: Venki Bathula <98110989+venkibric@users.noreply.github.com>

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/CriteriaService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/CriteriaService.java
@@ -10,7 +10,10 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.SerializationUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,7 @@ import uk.gov.crowncommercial.dts.scale.cat.exception.AgreementsServiceApplicati
 import uk.gov.crowncommercial.dts.scale.cat.exception.JaggaerApplicationException;
 import uk.gov.crowncommercial.dts.scale.cat.exception.ResourceNotFoundException;
 import uk.gov.crowncommercial.dts.scale.cat.mapper.DependencyMapper;
+import uk.gov.crowncommercial.dts.scale.cat.mapper.TimelineDependencyMapper;
 import uk.gov.crowncommercial.dts.scale.cat.model.agreements.DataTemplate;
 import uk.gov.crowncommercial.dts.scale.cat.model.agreements.Party;
 import uk.gov.crowncommercial.dts.scale.cat.model.agreements.Relationships;
@@ -56,6 +60,9 @@ public class CriteriaService {
   private final JaggaerAPIConfig jaggaerAPIConfig;
   private final WebClient jaggaerWebClient;
   private final DependencyMapper dependencyMapper;
+
+  private final TimelineDependencyMapper timelineDependencyMapper;
+
 
   private final DataTemplateProcessor templateProcessor;
   private final ProcurementEventHelperService eventHelperService;
@@ -145,7 +152,11 @@ public class CriteriaService {
     if(null != question.getNonOCDS() && null != question.getNonOCDS().getAnswered()){
       requirement.getNonOCDS().setAnswered(question.getNonOCDS().getAnswered());
     }
+    if(null != question.getNonOCDS().getTimelineDependency() && null != question.getNonOCDS().getTimelineDependency().getNonOCDS().getOptions()){
+             requirement.getNonOCDS().getTimelineDependency().getNonOCDS().updateOptions(getUpdatedOptions(question.getNonOCDS().getTimelineDependency().getNonOCDS().getOptions()));
+             requirement.getNonOCDS().getTimelineDependency().getNonOCDS().setAnswered(question.getNonOCDS().getTimelineDependency().getNonOCDS().getAnswered());
 
+    }
     validateQuestionsValues(group, requirement, options);
     requirement.getNonOCDS()
         .updateOptions(options.stream()
@@ -345,6 +356,9 @@ public class CriteriaService {
     if (Objects.nonNull(r.getNonOCDS().getDependency())) {
       questionNonOCDS.dependency(dependencyMapper.convertToQuestionNonOCDSDependency(r));
     }
+    if (Objects.nonNull(r.getNonOCDS().getTimelineDependency())) {
+      questionNonOCDS.timelineDependency(timelineDependencyMapper.convertToTimelineDependency(r));
+    }
 
     var description = r.getOcds().getDescription();
     if (Objects.nonNull(description) && description.contains(END_DATE)) {
@@ -383,5 +397,15 @@ public class CriteriaService {
               .titles(o.getTableDefinition().getTitles()).data(o.getTableDefinition().getData()));
     }
     return questionNonOCDSOptions;
+  }
+
+  private static List<Option> getUpdatedOptions(List<QuestionNonOCDSOptions> options) {
+    return options.stream()
+            .map(questionNonOCDSOptions -> Option.builder()
+                    .select(questionNonOCDSOptions.getSelected() == null ? Boolean.FALSE
+                            : questionNonOCDSOptions.getSelected())
+                    .value(questionNonOCDSOptions.getValue()).text(questionNonOCDSOptions.getText())
+                    .tableDefinition(questionNonOCDSOptions.getTableDefinition()).build())
+            .collect(Collectors.toList());
   }
 }
