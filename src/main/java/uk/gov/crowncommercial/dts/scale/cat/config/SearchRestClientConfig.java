b68271b699c197335715e02a9d08456bf1a872a0
Ravi <baby@ip-192-168-1-30.eu-west-2.compute.internal>
2023-07-13 15:15:23 +0100

    Updated the configuration mapping

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/SearchRestClientConfig.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/SearchRestClientConfig.java
@@ -9,9 +9,11 @@ import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.data.client.orhlc.AbstractOpenSearchConfiguration;
 import org.opensearch.data.client.orhlc.ClientConfiguration;
 import org.opensearch.data.client.orhlc.RestClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import uk.gov.crowncommercial.dts.scale.cat.config.paas.AWSS3Service;
 import uk.gov.crowncommercial.dts.scale.cat.config.paas.OpensearchCredentials;
 import uk.gov.crowncommercial.dts.scale.cat.config.paas.OpensearchService;
@@ -19,6 +21,7 @@ import uk.gov.crowncommercial.dts.scale.cat.config.paas.VCAPServices;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import java.util.Arrays;
 
 @Configuration
 
@@ -27,23 +30,35 @@ public class SearchRestClientConfig extends AbstractOpenSearchConfiguration {
     static final String OPEN_SEARCH_PATTERN = "[a-z0-9]+-ccs-scale-cat-opensearch";
     private final VCAPServices vcapServices;
 
+    @Autowired
+    Environment environment;
+
 
     @SneakyThrows
     @Override
     @Bean
     public RestHighLevelClient opensearchClient() {
-
+        ClientConfiguration clientConfiguration;
          SSLContext sslContext = SSLContexts.custom()
                 .loadTrustMaterial(null, TrustAllStrategy.INSTANCE)
                 .build();
         OpensearchCredentials opensearchCredentials = vcapServices.getOpensearch().stream()
                 .filter(b -> b.getName().matches(OPEN_SEARCH_PATTERN)).findFirst().orElseThrow().getCredentials();
         String hostnameurl = opensearchCredentials.getHostname()+":"+opensearchCredentials.getPort();
-        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo(hostnameurl)
-                .usingSsl(sslContext, NoopHostnameVerifier.INSTANCE)
-                .withBasicAuth(opensearchCredentials.getUsername(),opensearchCredentials.getPassword())
-                .build();
+
+        if(Arrays.stream(environment.getActiveProfiles()).filter(profile -> profile.contains("local")).findFirst().isPresent())
+        {
+            clientConfiguration = ClientConfiguration.builder()
+                    .connectedTo(hostnameurl)
+                    .build();
+
+        }else {
+            clientConfiguration = ClientConfiguration.builder()
+                    .connectedTo(hostnameurl)
+                    .usingSsl(sslContext, NoopHostnameVerifier.INSTANCE)
+                    .withBasicAuth(opensearchCredentials.getUsername(), opensearchCredentials.getPassword())
+                    .build();
+        }
 
         return RestClients.create(clientConfiguration).rest();
     }
