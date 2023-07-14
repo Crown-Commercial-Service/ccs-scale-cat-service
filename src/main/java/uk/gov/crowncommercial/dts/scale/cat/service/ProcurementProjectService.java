bc98ba103276d4ef1839d21bbe4ef2eb33d4459d
Ravi <baby@ip-192-168-1-30.eu-west-2.compute.internal>
2023-07-14 14:29:25 +0100

    Updated the lot count

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ProcurementProjectService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ProcurementProjectService.java
@@ -873,9 +873,9 @@ public class ProcurementProjectService {
   }
 
   private  NativeSearchQuery getLotCount (String keyword, String lotId, ProjectFilter projectFilter) {
-    NativeSearchQueryBuilder searchQueryBuilder = getFilterQuery(lotId,projectFilter, keyword);
+    NativeSearchQueryBuilder searchQueryBuilder = getFilterQuery(null,projectFilter, keyword);
     searchQueryBuilder.withPageable(PageRequest.of(0, (int) searchProjectRepo.count()));
-      searchQueryBuilder.withAggregations(AggregationBuilders.terms(COUNT_AGGREGATION).field("lot.keyword").size(100));
+      searchQueryBuilder.withAggregations(AggregationBuilders.terms(COUNT_AGGREGATION).field("lot.raw").size(100));
     NativeSearchQuery searchQuery = searchQueryBuilder.build();
     return searchQuery;
   }
