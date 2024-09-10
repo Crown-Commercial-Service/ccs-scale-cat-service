fcaa285d58118b13914f433d1cf0cff8cf882f11
NToppingCCS <nathan.topping@crowncommercial.gov.uk>
2024-09-10 11:19:14 +0100

    Rollback breaking change

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/Application.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/Application.java
@@ -5,15 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import lombok.RequiredArgsConstructor;
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
-import uk.gov.crowncommercial.dts.scale.cat.repo.search.SearchProjectRepo;
 
 @SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
 @RequiredArgsConstructor
@@ -21,9 +15,6 @@ import uk.gov.crowncommercial.dts.scale.cat.repo.search.SearchProjectRepo;
 @EnableAsync
 @EnableCaching
 @EnableFeignClients
-@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
-@EnableElasticsearchRepositories(basePackages= {"uk.gov.crowncommercial.dts.scale.cat.repo.search"})
-@EnableJpaRepositories(basePackages = {"uk.gov.crowncommercial.dts.scale.cat.repo"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {SearchProjectRepo.class})})
 public class Application {
   public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
