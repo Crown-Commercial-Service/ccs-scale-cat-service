fd8ee8c6d56498c89c74dd79737f31f0c83cd589
NToppingCCS <nathan.topping@crowncommercial.gov.uk>
2024-11-26 11:12:31 +0000

    Start by cleaning up the core mapping entry point, and adding some comments

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ocds/OcdsRecordMapper.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/ocds/OcdsRecordMapper.java
@@ -11,6 +11,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Looks to map OCDS question and answer data to fixed data structs for Project Detail fetches
+ */
 @Component
 @RequiredArgsConstructor
 public class OcdsRecordMapper  implements InitializingBean {
@@ -21,123 +24,92 @@ public class OcdsRecordMapper  implements InitializingBean {
     private final CompiledReleaseAwardsService compiledAwardsService;
     private final StatisticsService statisticsService;
 
-
+    /**
+     * Runs all necessary population requests for the data record in an async manner via CompletableFutures
+     * This is the core method run when the mapper is used
+     */
     public Record1 populate(ProjectQuery query, Record1 record){
+        // Worth noting this is not the appropriate way to use CompletableFutures so we should look to refactor this when possible
         Record1 result = record;
         List<CompletableFuture> cfs = new ArrayList<>();
-        for(String section: query.getSections()){
-            ProjectRecordHandler handler = handlers.get(section);
-            if(null != handler){
-                MapperResponse response = handler.apply(query, result);
-                result = response.getRecord();
-                if(null != response.getCompletableFuture()){
-                    cfs.add(response.getCompletableFuture());
+
+        // For each section of the requested query object, run all relevant mappings dictated by the handlers config
+        if (query != null && query.getSections() != null && !query.getSections().isEmpty()) {
+            for (String section : query.getSections()) {
+                ProjectRecordHandler handler = handlers.get(section);
+
+                if (handler != null) {
+                    MapperResponse response = handler.apply(query, result);
+                    result = response.getRecord();
+
+                    if (response.getCompletableFuture() != null) {
+                        cfs.add(response.getCompletableFuture());
+                    }
                 }
             }
         }
-        if(cfs.size() > 0) {
-            CompletableFuture[] sdf = cfs.toArray(new CompletableFuture[cfs.size()]);
+
+        if(!cfs.isEmpty()) {
+            // Wait for any aync requests to complete before we move on
+            CompletableFuture[] sdf = cfs.toArray(new CompletableFuture[0]);
             CompletableFuture.allOf(sdf).join();
         }
 
         return result;
     }
 
+    /**
+     * Sets up the handlers configuration for this mapping service, so that requests know what it is to try and map
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
+        // Define via handlers all of the mapping tasks that should be run for a request
         handlers = new HashMap<>();
 
-        handlers.put(OcdsSections.COMPILED_RELEASE, (pq, re) -> {
-            return releaseService.populate(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_PARTIES, (pq, re) -> {
-            return releaseService.populateParties(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_BUYER, (pq, re) -> {
-            return releaseService.populateBuyer(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_CONTRACTS, (pq, re) -> {
-            return releaseService.populateContracts(re, pq);
-        });
+        handlers.put(OcdsSections.COMPILED_RELEASE, (pq, re) -> releaseService.populate(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_PARTIES, (pq, re) -> releaseService.populateParties(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_BUYER, (pq, re) -> releaseService.populateBuyer(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_CONTRACTS, (pq, re) -> releaseService.populateContracts(re, pq));
 
         mapCompiledReleasePlanning();
-
         mapCompiledReleaseTender();
-
         mapCompiledReleaseAward();
-
         mapCompiledReleaseStatistics();
     }
 
+    /**
+     * Configure handler instructions for statistics mapping tasks
+     */
     private void mapCompiledReleaseStatistics() {
-        handlers.put(OcdsSections.COMPILED_RELEASE_STATISTICS,  (pq, re) -> {
-            return statisticsService.populate(re, pq);
-        });
+        handlers.put(OcdsSections.COMPILED_RELEASE_STATISTICS,  (pq, re) -> statisticsService.populate(re, pq));
     }
 
+    /**
+     * Configure handler instructions for planning mapping tasks
+     */
     private void mapCompiledReleasePlanning() {
-        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING,  (pq, re) -> {
-            return compiledPlanningService.populateGeneral(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING_BUDGET,  (pq, re) -> {
-            return compiledPlanningService.populateBudget(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING_DOCUMENTS,  (pq, re) -> {
-            return compiledPlanningService.populateDocuments(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING_MILESTONES,  (pq, re) -> {
-            return compiledPlanningService.populateMilestones(re, pq);
-        });
+        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING,  (pq, re) -> compiledPlanningService.populateGeneral(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING_BUDGET,  (pq, re) -> compiledPlanningService.populateBudget(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING_DOCUMENTS,  (pq, re) -> compiledPlanningService.populateDocuments(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_PLANNING_MILESTONES,  (pq, re) -> compiledPlanningService.populateMilestones(re, pq));
     }
 
+    /**
+     * Configure handler instructions for release tender mapping tasks
+     */
     private void mapCompiledReleaseTender() {
-        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER,  (pq, re) -> {
-            return compiledTenderService.populateGeneral(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_TENDERERS,  (pq, re) -> {
-            return compiledTenderService.populateTenderers(re, pq);
-        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_DOCUMENTS,  (pq, re) -> {
-//            return compiledTenderService.populateDocuments(re, pq);
-//        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_MILESTONES,  (pq, re) -> {
-//            return compiledTenderService.populateMilestones(re, pq);
-//        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_AMENDMENTS,  (pq, re) -> {
-//            return compiledTenderService.populateAmendments(re, pq);
-//        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_ENQUIRIES,  (pq, re) -> {
-            return compiledTenderService.populateEnquiries(re, pq);
-        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_CRITERIA,  (pq, re) -> {
-            return compiledTenderService.populateCriteria(re, pq);
-        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_SELECTIONCRITERIA,  (pq, re) -> {
-//            return compiledTenderService.populateSelectionCriteria(re, pq);
-//        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_TECHNIQUES,  (pq, re) -> {
-//            return compiledTenderService.populateTechniques(re, pq);
-//        });
+        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER,  (pq, re) -> compiledTenderService.populateGeneral(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_TENDERERS,  (pq, re) -> compiledTenderService.populateTenderers(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_ENQUIRIES,  (pq, re) -> compiledTenderService.populateEnquiries(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_TENDER_CRITERIA,  (pq, re) -> compiledTenderService.populateCriteria(re, pq));
     }
 
+    /**
+     * Configure handler instructions for release award mapping tasks
+     */
     private void mapCompiledReleaseAward() {
-        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS,  (pq, re) -> {
-            return compiledAwardsService.populateGeneral(re, pq);
-        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS_SUPPLIERS,  (pq, re) -> {
-//            return compiledAwardsService.populateSuppliers(re, pq);
-//        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS_ITEMS,  (pq, re) -> {
-            return compiledAwardsService.populateItems(re, pq);
-        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS_DOCUMENTS,  (pq, re) -> {
-//            return compiledAwardsService.populateDocuments(re, pq);
-//        });
-//        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS_AMENDMENTS,  (pq, re) -> {
-//            return compiledAwardsService.populateAmendments(re, pq);
-//        });
-        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS_REQRESPONSES,  (pq, re) -> {
-            return compiledAwardsService.populateRequiremetResponses(re, pq);
-        });
+        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS,  (pq, re) -> compiledAwardsService.populateGeneral(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS_ITEMS,  (pq, re) -> compiledAwardsService.populateItems(re, pq));
+        handlers.put(OcdsSections.COMPILED_RELEASE_AWARDS_REQRESPONSES,  (pq, re) -> compiledAwardsService.populateRequiremetResponses(re, pq));
     }
 }
