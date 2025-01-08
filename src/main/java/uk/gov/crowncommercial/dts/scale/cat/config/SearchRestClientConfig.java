a7e6f68edbaf421af68e708860438a6e966c1aef
NToppingCCS <nathan.topping@crowncommercial.gov.uk>
2025-01-08 10:39:59 +0000

    Fix ElastiCache config for local running

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/SearchRestClientConfig.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/config/SearchRestClientConfig.java
@@ -10,6 +10,7 @@ import org.opensearch.data.client.orhlc.AbstractOpenSearchConfiguration;
 import org.opensearch.data.client.orhlc.ClientConfiguration;
 import org.opensearch.data.client.orhlc.RestClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -17,10 +18,8 @@ import uk.gov.crowncommercial.dts.scale.cat.config.paas.OpensearchCredentials;
 import uk.gov.crowncommercial.dts.scale.cat.config.paas.VCAPServices;
 
 import javax.net.ssl.SSLContext;
-import java.util.Arrays;
 
 @Configuration
-
 @Data
 public class SearchRestClientConfig extends AbstractOpenSearchConfiguration {
     static final String OPEN_SEARCH_PATTERN = "[a-z0-9]+-ccs-scale-cat-opensearch";
@@ -29,6 +28,8 @@ public class SearchRestClientConfig extends AbstractOpenSearchConfiguration {
     @Autowired
     Environment environment;
 
+    @Value("${config.flags.devMode}")
+    boolean devMode;
 
     @SneakyThrows
     @Override
@@ -44,8 +45,10 @@ public class SearchRestClientConfig extends AbstractOpenSearchConfiguration {
         ClientConfiguration.MaybeSecureClientConfigurationBuilder clientConfigurationBuilder = ClientConfiguration.builder()
             .connectedTo(hostnameurl);
 
-        // Enforce use of SSL
-        clientConfigurationBuilder.usingSsl(sslContext, NoopHostnameVerifier.INSTANCE);
+        // Enforce use of SSL when not on a local environment
+        if (!devMode) {
+            clientConfigurationBuilder.usingSsl(sslContext, NoopHostnameVerifier.INSTANCE);
+        }
 
         clientConfiguration = clientConfigurationBuilder.build();
 
