1961a3a5492e2d3d46ff413c124c0b72f63481c5
SamB-CCS <sam.boothroyd@crowncommercial.gov.uk>
2025-03-14 11:50:38 +0000

    increase scheduling time for PROCESSING status update

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/documentupload/DocumentUploadScheduledTask.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/documentupload/DocumentUploadScheduledTask.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import uk.gov.crowncommercial.dts.scale.cat.model.entity.VirusCheckStatus;
 import uk.gov.crowncommercial.dts.scale.cat.repo.DocumentUploadRepo;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  *
  */
@@ -16,18 +18,34 @@ import uk.gov.crowncommercial.dts.scale.cat.repo.DocumentUploadRepo;
 @Slf4j
 public class DocumentUploadScheduledTask {
 
-  static final String PRINCIPAL = "TENDERS_API_DOC_UPLOAD_DEAMON";
+    static final String PRINCIPAL = "TENDERS_API_DOC_UPLOAD_DEAMON";
 
-  private final DocumentUploadService documentUploadService;
-  private final DocumentUploadRepo documentUploadRepo;
+    private final ReentrantLock schedulerLock = new ReentrantLock();
+    private final DocumentUploadService documentUploadService;
+    private final DocumentUploadRepo documentUploadRepo;
 
-  @Scheduled(fixedDelayString = "PT1M")
-  @Transactional
-  void checkDocumentStatus() {
-    var unprocessedDocuments = documentUploadRepo.findByExternalStatus(VirusCheckStatus.PROCESSING);
-    log.debug("Begin scheduled processing of {} unprocessed documents",
-        unprocessedDocuments.size());
-    documentUploadService.processDocuments(unprocessedDocuments, PRINCIPAL);
-  }
-
+    @Scheduled(fixedDelayString = "PT10S")
+    @Transactional
+    void checkDocumentStatus() {
+        if (schedulerLock.tryLock()) {
+            try {
+                var unprocessedDocuments = documentUploadRepo.findByExternalStatus(VirusCheckStatus.PROCESSING);
+                log.debug("Begin scheduled processing of {} unprocessed documents",
+                        unprocessedDocuments.size());
+                documentUploadService.processDocuments(unprocessedDocuments, PRINCIPAL);
+            } finally {
+                schedulerLock.unlock();
+            }
+        } else {
+            log.debug("Previous task still running, skipping this execution");
+        }
+    }
 }
+// void checkDocumentStatus() {
+//    var unprocessedDocuments = documentUploadRepo.findByExternalStatus(VirusCheckStatus.PROCESSING);
+//    log.debug("Begin scheduled processing of {} unprocessed documents",
+//        unprocessedDocuments.size());
+//    documentUploadService.processDocuments(unprocessedDocuments, PRINCIPAL);
+//  }
+
+
