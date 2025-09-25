8dbc047a3a16dab46db499f03e554739b81edf27
Zahid Anwar <zahid.anwar@Zahids-MacBook-Pro-business.local>
2025-09-25 12:04:30 +0100

    Refactor code to return audit logs with date and timestamp

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/AuditLogService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/AuditLogService.java
@@ -1,6 +1,9 @@
 package uk.gov.crowncommercial.dts.scale.cat.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import uk.gov.crowncommercial.dts.scale.cat.model.entity.audit.AuditLog;
 import uk.gov.crowncommercial.dts.scale.cat.model.entity.audit.AuditLogDto;
@@ -11,8 +14,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Audit log service to handle CRUD operations
@@ -27,9 +29,13 @@ public class AuditLogService {
     private static final String DEFAULT_AUDIT_REASON = "No audit reason specified";
     private static final String DEFAULT_AUDIT_FROM_URL = "No audit from url specified";
     private static final String DEFAULT_AUDIT_UPDATED_BY = "No audit reason specified";
+    private static final Logger log = LoggerFactory.getLogger(AuditLogService.class);
 
     private final DateTimeFormatter formatter =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+    private final DateTimeFormatter formatterForParamsDate =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
 
     @Autowired
     private AuditLogRepo repository;
@@ -38,8 +44,14 @@ public class AuditLogService {
     public List<AuditLogDto> getAuditLogsWithDate(String fromDate,
                                                   String toDate) {
 
+        //TODO Pagination and filtering data should be implemented in the upcoming sprint
+        log.debug("Fetching audit logs, fromDate: {}, toDate: {}", fromDate, toDate);
+        LocalDateTime localDateTimeFromDate = LocalDateTime.parse(fromDate, formatterForParamsDate);
+        LocalDateTime localDateTimeToDate = LocalDateTime.parse(toDate, formatterForParamsDate);
         return repository
-                .findAll()
+                .findByTimestampBetweenOrderByTimestampDesc(
+                Timestamp.valueOf(localDateTimeFromDate),
+                Timestamp.valueOf(localDateTimeToDate))
                 .stream()
                 .map(this::mapModelToDto)
                 .toList();
