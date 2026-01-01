6519265f3665fdff088984dc1dc9746da2de1dff
Tom <60753515+tberey@users.noreply.github.com>
2026-01-01 14:08:42 +0000

    Update DocumentTemplatesControllerTest.java

--- src/test/java/uk/gov/crowncommercial/dts/scale/cat/controller/DocumentTemplatesControllerTest.java
+++ src/test/java/uk/gov/crowncommercial/dts/scale/cat/controller/DocumentTemplatesControllerTest.java
@@ -22,7 +22,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.javacrumbs.shedlock.core.LockProvider;
