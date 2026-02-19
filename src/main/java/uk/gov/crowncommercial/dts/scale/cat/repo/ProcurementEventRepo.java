ac8b5b393a6b393370a0864c6526326b67fa3d12
Tom <60753515+tberey@users.noreply.github.com>
2026-02-19 12:16:22 +0000

    Update ProcurementEventRepo.java

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/repo/ProcurementEventRepo.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/repo/ProcurementEventRepo.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.crowncommercial.dts.scale.cat.model.entity.ProcurementEvent;
 
