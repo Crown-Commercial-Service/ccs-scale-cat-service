ae7a259fbea4f32b6b9b01969e3c33efe38a553e
vipul.lakhani <vipul.lakhani@crowncommercial.gov.uk>
2026-02-26 16:38:14 +0000

    NCAS-1221: Lot3 CAS-API changes

---
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/DigitalRoleService.java
@@ -0,0 +1,46 @@
+package uk.gov.crowncommercial.dts.scale.cat.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uk.gov.crowncommercial.dts.scale.cat.model.entity.DigitalRole;
+import uk.gov.crowncommercial.dts.scale.cat.repo.DigitalRoleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DigitalRoleService {
+
+  private final DigitalRoleRepository digitalRoleRepository;
+
+  @Transactional(readOnly = true)
+  public Optional<DigitalRole> findById(final Long id) {
+    return digitalRoleRepository.findById(id);
+  }
+
+  @Transactional(readOnly = true)
+  public List<DigitalRole> findAllByProjectIdAndEventId(
+      final String projectId, final String eventId) {
+    return digitalRoleRepository.findAllByProjectIdAndEventIdOrderByJobFamilyAscRoleAscLevelAsc(
+        projectId, eventId);
+  }
+
+  @Transactional
+  public DigitalRole save(final DigitalRole entity) {
+    return digitalRoleRepository.save(entity);
+  }
+
+  @Transactional
+  public List<DigitalRole> saveAll(final List<DigitalRole> entities) {
+    return digitalRoleRepository.saveAll(entities);
+  }
+
+  @Transactional
+  public void delete(final DigitalRole entity) {
+    digitalRoleRepository.delete(entity);
+  }
+}
