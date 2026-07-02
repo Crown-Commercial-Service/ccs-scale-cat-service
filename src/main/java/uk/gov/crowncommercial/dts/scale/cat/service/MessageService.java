643aa4f396e91d3e3a4eadcff04ccccba9caab7d
Zahid Anwar <zahid.anwar@crowncommercial.gov.uk>
2026-07-02 14:09:50 +0100

    Removed messaging logs from jaggaer service and message service

--- src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/MessageService.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/cat/service/MessageService.java
@@ -125,17 +125,12 @@ public class MessageService {
 
     // REM Assumption that user is buyer only
 
-    log.debug("Calling Jaggaer to get messages - (getMessagesSummary)");
     var jaggaerUserId = userProfileService
         .resolveBuyerUserProfile(messageRequestInfo.getPrincipal())
         .orElseThrow(() -> new AuthorisationFailureException(JAGGAER_USER_NOT_FOUND)).getUserId();
 
-    log.debug("Got jaggaer user Id");
-
     var event = validationService.validateProjectAndEventIds(messageRequestInfo.getProcId(),
         messageRequestInfo.getEventId(), null);
-
-    log.debug("Validated event");
 
     Predicate<uk.gov.crowncommercial.dts.scale.cat.model.jaggaer.Message> directionPredicate =
         message -> (MessageDirection.ALL.equals(messageRequestInfo.getMessageDirection())
@@ -148,13 +143,6 @@ public class MessageService {
     var messagesResponse =
         jaggaerService.getMessages(event.getExternalReferenceId(), 1);
     var allMessages = messagesResponse.getMessageList().getMessage();
-
-    log.debug("Called Jaggaer successfully to retrieve message/messages.");
-
-    if(messagesResponse != null) {
-        log.info("Jaggaer message response, returnCode: {}, returnMessage: {}, total messages: {}",
-                messagesResponse.getReturnCode(), messagesResponse.getReturnMessage(), allMessages.size());
-    }
 
     /**
      * Make first request to jagger if total records are more than > 100 (Jaggaer returns max 100
@@ -176,13 +164,6 @@ public class MessageService {
         .filter(message -> (isEmpty(message.getReceiverList().getReceiver())
             || message.getReceiverList().getReceiver().stream().anyMatch(receiverPredicate)))
         .collect(Collectors.toList());
-
-      log.debug("Called Jaggaer successfully to retrieve message/messages. and total messages");
-
-      if(messages != null) {
-          log.info("Jaggaer total number of messages: {}",
-                  messages.size());
-      }
 
     if (messages.isEmpty()) {
       return new MessageSummary();
